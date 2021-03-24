3b0bc1459924bed517058dfdcb962cf7fbc5fd11
Pratiti Bera <remabimal0801@gmail.com>
2021-03-24 16:31:00 +0530

    Extension to page

--- PageFontStyle/content.js
+++ PageFontStyle/content.js
@@ -1,9 +1,10 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if (request.todo == "changeColor"){
-        var addColor = '#' + request.clickedColor;               
+        var addColor = request.clickedColor;
+        var elementselected = request.clickedElement;               
         $('.inputfield').css('font-style','italic');
-         $('#avalue').val(addColor);
+         $('#' + elementselected + 'value').val(addColor);
 
          // var v = $('.input').val(addColor);
 
